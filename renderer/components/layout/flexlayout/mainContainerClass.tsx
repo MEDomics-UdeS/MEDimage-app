a75fb467765b41b9c5753d3821570822d4699bc1
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-02-23 17:10:22 -0500

    very minor improvements!

--- renderer/components/layout/flexlayout/mainContainerClass.tsx
+++ renderer/components/layout/flexlayout/mainContainerClass.tsx
@@ -886,7 +886,7 @@ class MainInnerContainer extends React.Component<any, { layoutFile: string | nul
       let icon = <span style={{ marginRight: 3 }}>{iconToReturn}</span>
       return icon
     } else {
-      if (component === "InputToolsDB" || component === "inputPage") {
+      if (component === "InputToolsDB" || component === "inputPage" || component === "dataTableFromDB") {
         return <span style={{ marginRight: 3 }}>🛢️</span>
       }
       if (component === "BatchExtractor") {
