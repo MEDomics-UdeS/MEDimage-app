e997c11b3ca8ef270c220f83be5b7bf29593d05b
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-02-25 11:01:58 -0500

    Added feature to clone nodes

--- renderer/components/extractionMEDiml/flowCanvas.jsx
+++ renderer/components/extractionMEDiml/flowCanvas.jsx
@@ -3,6 +3,7 @@ import React, { useCallback, useContext, useEffect, useMemo, useState } from "re
 import { toast } from "react-toastify"
 
 // Import utilities
+import uuid from "react-native-uuid"
 import { loadJsonSync, processBatchSettings } from "../../utilities/fileManagementUtils"
 import { requestBackend } from "../../utilities/requests"
 import ProgressBarRequests from "../generalPurpose/progressBarRequests"
@@ -404,6 +405,23 @@ const FlowCanvas = ({ workflowType, setWorkflowType }) => {
     }
 
     return newNode
+  }
+
+  const duplicateNode = (id) => {
+    const nodeToDuplicate = nodes.find((node) => node.id === id)
+    if (!nodeToDuplicate) return
+
+    const newNode = {
+      ...deepCopy(nodeToDuplicate),
+      id: `node_${uuid.v4()}`,
+      position: {
+        x: nodeToDuplicate.position.x + 40,
+        y: nodeToDuplicate.position.y + 100
+      },
+      selected: false
+    }
+
+    setNodes((nds) => [...nds, newNode])
   }
 
   /**
@@ -998,6 +1016,7 @@ const FlowCanvas = ({ workflowType, setWorkflowType }) => {
           reactFlowInstance: reactFlowInstance,
           setReactFlowInstance: setReactFlowInstance,
           addSpecificToNode: addSpecificToNode,
+          duplicateNode: duplicateNode,
           nodeTypes: nodeTypes,
           nodes: nodes,
           setNodes: setNodes,
