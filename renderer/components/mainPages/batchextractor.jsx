e72d32f9025bbc78e269cc6c8ec6ed6989e26436
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-02-25 10:45:24 -0500

    UI/UX improvements

--- renderer/components/mainPages/batchextractor.jsx
+++ renderer/components/mainPages/batchextractor.jsx
@@ -48,6 +48,7 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
   const [showRadiomicsResults, setShowRadiomicsResults] = useState(false) // used to display the extraction results
   const [showEdit, setShowEdit] = useState(false) // used to display the extraction results
   const [nodes, setNodes] = useState([])
+  const [useWorkspace, setUseWorkspace] = useState(true) // A boolean variable to control the use of the workspace
 
   useEffect(() => {
     updateWSfolder()
@@ -486,28 +487,30 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
         </Card.Header>
 
       <Form method="post" encType="multipart/form-data" className="inputFile">
+
+      {/* Check whether to use the workspace or not*/}
+      <Row className="form-group-box">
+        <Form.Label htmlFor="file">Use current workspace data (recommanded)</Form.Label>
+        <i>If this is checked, the data available in the workspace will be used instead of local data.</i>
+        <Col style={{ width: "150px" }}>
+          <InputSwitch
+            checked={useWorkspace}
+            onChange={(e) => setUseWorkspace(e.value)}
+          />
+        </Col>
+      </Row>
+
       {/* UPLOAD NPY DATASET FOLDER*/}
-        <Row className="form-group-box">
-          <Tooltip target=".npy-folder"/>
-          <Form.Label 
-            className="npy-folder" 
-            data-pr-tooltip="Path to the folder containing the NPY dataset to use for radiomics features extraction"
-            data-pr-position="bottom"
-            htmlFor="file">
-              NPY dataset folder (MEDscan objects)
-          </Form.Label>
-          <Col>
-            <h6>Select a local foler</h6>
-            <Form.Group controlId="enterFile">
-              <Form.Control
-                name="path_read"
-                type="file"
-                webkitdirectory="true"
-                directory="true"
-                onChange={handleReadFolderChange}
-              />
-            </Form.Group>
-          </Col>
+      <Row className="form-group-box">
+        <Tooltip target=".npy-folder"/>
+        <Form.Label 
+          className="npy-folder" 
+          data-pr-tooltip="Path to the folder containing the NPY dataset to use for radiomics features extraction"
+          data-pr-position="bottom"
+          htmlFor="file">
+            NPY dataset folder (MEDscan objects)
+        </Form.Label>
+        {useWorkspace ? (
           <Col>
             <Tooltip target=".csv-file-ws"/>
             <h6>Select from workspace</h6>
@@ -522,7 +525,21 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
               placeholder="Select a folder"
             />
           </Col>
-        </Row>
+            ) : (
+          <Col>
+            <h6>Select a local foler</h6>
+            <Form.Group controlId="enterFile">
+              <Form.Control
+                name="path_read"
+                type="file"
+                webkitdirectory="true"
+                directory="true"
+                onChange={handleReadFolderChange}
+              />
+            </Form.Group>
+          </Col>
+        )}
+      </Row>
 
         {/* UPLOAD SETTINGS FILE*/}
         <Row className="form-group-box">
@@ -534,47 +551,51 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
             htmlFor="file">
               Settings File
           </Form.Label>
-          <Col>
-            <h6>Load a Local File</h6>
-            <Form.Group controlId="enterFile">
-              <Form.Control
-                accept='.json'
-                name="path_params"
-                type="file"
-                onChange={handleSettingsFileChange}
+          {useWorkspace ? (
+            <Col>
+              <Tooltip target=".csv-file-ws"/>
+              <h6>Select From Workspace</h6>
+              <Dropdown
+                style={{ maxWidth: "100%", height: "auto", width: "auto" }}
+                filter
+                value={selectedSettingsFile}
+                onChange={(e) => setSelectedSettingsFile(e.value)}
+                options={listSettingsFiles}
+                optionLabel="name"
+                display="chip"
+                placeholder="Select a file"
               />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Tooltip target=".csv-file-ws"/>
-            <h6>Select From Workspace</h6>
-            <Dropdown
-              style={{ maxWidth: "100%", height: "auto", width: "auto" }}
-              filter
-              value={selectedSettingsFile}
-              onChange={(e) => setSelectedSettingsFile(e.value)}
-              options={listSettingsFiles}
-              optionLabel="name"
-              display="chip"
-              placeholder="Select a file"
-            />
-          </Col>
-          <Col>
-          <h6>Edit the selected file</h6>
-            <Button
-              type="button"
-              severity="info"
-              label="Edit"
-              name="EditSettingsButton"
-              onClick={handleEditClick}
-              disabled={(!selectedSettingsFile)}
-              loading={loadingEdit}
-              icon="pi pi-pencil"
-              iconPos="left"
-              raised
-              rounded
-            />
-          </Col>
+            </Col>
+             ) : (
+            <Col>
+              <h6>Load a Local File</h6>
+              <Form.Group controlId="enterFile">
+                <Form.Control
+                  accept='.json'
+                  name="path_params"
+                  type="file"
+                  onChange={handleSettingsFileChange}
+                />
+              </Form.Group>
+            </Col>
+            
+            )}
+            <Col>
+            <h6>Edit the selected file</h6>
+              <Button
+                type="button"
+                severity="info"
+                label="Edit"
+                name="EditSettingsButton"
+                onClick={handleEditClick}
+                disabled={(!selectedSettingsFile)}
+                loading={loadingEdit}
+                icon="pi pi-pencil"
+                iconPos="left"
+                raised
+                rounded
+              />
+            </Col>
         </Row>
       </Form>  
 
@@ -588,18 +609,7 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
           htmlFor="file">
             Path to CSV File
         </Form.Label>
-          <Col>
-            <h6>Select a Local File</h6>
-            <Form.Group controlId="enterFile">
-              <Form.Control
-                name="path_csv"
-                type="file"
-                webkitdirectory="true"
-                directory="true"
-                onChange={handleCSVFileChange}
-              />
-            </Form.Group>
-          </Col>
+        {useWorkspace ? (
           <Col>
             <Tooltip target=".csv-file-ws"/>
             <h6>Select From Workspace</h6>
@@ -613,7 +623,20 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
               display="chip"
               placeholder="Select a file"
             />
+          </Col> ) :(
+          <Col>
+            <h6>Select a Local File</h6>
+            <Form.Group controlId="enterFile">
+              <Form.Control
+                name="path_csv"
+                type="file"
+                webkitdirectory="true"
+                directory="true"
+                onChange={handleCSVFileChange}
+              />
+            </Form.Group>
           </Col>
+          )}
         </Row>
 
         {/* UPLOAD SAVING FOLDER*/}
@@ -626,18 +649,7 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
             htmlFor="file">
               Save folder
           </Form.Label>
-          <Col>
-          <h6>Select a Local Folder</h6>
-            <Form.Group controlId="enterFile">
-              <Form.Control
-                name="path_save"
-                type="file"
-                webkitdirectory="true"
-                directory="true"
-                onChange={handleSaveFolderChange}
-              />
-            </Form.Group>
-          </Col>
+        {useWorkspace ? (
           <Col>
             <Tooltip target=".csv-file-ws"/>
             <h6>Select From Workspace</h6>
@@ -651,7 +663,20 @@ const BatchExtractor = ({ pageId, configPath = "" }) => {
               display="chip"
               placeholder="Select a folder"
             />
+          </Col> ) :(
+          <Col>
+          <h6>Select a Local Folder</h6>
+            <Form.Group controlId="enterFile">
+              <Form.Control
+                name="path_save"
+                type="file"
+                webkitdirectory="true"
+                directory="true"
+                onChange={handleSaveFolderChange}
+              />
+            </Form.Group>
           </Col>
+          )}
         </Row>
 
       {/* NUMBER OF BATCH*/}
