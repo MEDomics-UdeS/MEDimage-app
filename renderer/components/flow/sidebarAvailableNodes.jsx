e8eed23ef346df8c4e58fa8025eae908957313cf
MAHDI ALL <mehdiwinneri01@gmail.com>
2026-05-08 09:08:10 -0400

    Some code cleaning and minor UI UX improvs

--- renderer/components/flow/sidebarAvailableNodes.jsx
+++ renderer/components/flow/sidebarAvailableNodes.jsx
@@ -22,7 +22,7 @@ const onDragStart = (event, node) => {
 }
 
 const SectionContainer = ({ title, children }) => (
-  <div className="card mb-3" style={{backgroundColor: "#dbecf9"}}>
+  <div className="card mb-3" style={{backgroundColor: "#dbecf9", borderBottomColor: "#b9b9b9"}}>
     <h6 className="section-header p-2 border-bottom">
       {title}
     </h6>
