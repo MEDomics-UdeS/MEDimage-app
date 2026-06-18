170a465daa32163298eb1fd361293c53a0491425
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-06-18 16:09:15 -0400

    UI improvement of the home page

--- renderer/components/mainPages/home.jsx
+++ renderer/components/mainPages/home.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect, useState } from "react"
-import Image from "next/image"
-import myimage from "../../../resources/medomics_transparent_bg.png"
-import { Button, Stack } from "react-bootstrap"
-import { WorkspaceContext } from "../workspace/workspaceContext"
 import { ipcRenderer } from "electron"
+import { Check, FileText, MonitorPlay, Play, TriangleAlert } from 'lucide-react'
+import Image from "next/image"
+import { useContext, useEffect, useState } from "react"
+import { Button, Stack } from "react-bootstrap"
+import myimage from "../../../resources/medomics_transparent_bg.png"
 import FirstSetupModal from "../generalPurpose/installation/firstSetupModal"
+import { WorkspaceContext } from "../workspace/workspaceContext"
 
 /**
  *
@@ -66,7 +67,6 @@ const HomePage = () => {
         }}
       >
         <Stack direction="vertical" gap={1} style={{ alignContent: "center", flexGrow: 1 }}>
-          <h2>Home page</h2>
           <Stack direction="horizontal" gap={0} style={{ padding: "0 0 0 0", alignContent: "center" }}>
             <h1 style={{ fontSize: "5rem" }}>MEDiml</h1>
             <h2 style={{ fontSize: "2rem", marginTop: "2.5rem" }}>v{appVersion}</h2>
@@ -102,7 +102,22 @@ const HomePage = () => {
               </Stack>
             </>
           ) : (
-            <h5>Workspace is set to {workspace.workingDirectory.path}</h5>
+            <h5
+              style={{
+                color: "#f7faff",
+                backgroundColor: "#4475be",
+                border: "1px solid #ebf6ff",
+                borderLeft: "6px solid #4475be",
+                padding: "12px 18px",
+                borderRadius: "10px",
+                marginTop: "1rem",
+                width: "fit-content",
+                boxShadow: "0 2px 8px rgba(0, 0, 0, 0.08)",
+                margin: "0.5rem",
+              }}
+            >
+              <Check size={20} className="mb-1 mr-4 inline-block" /> Workspace is set to {workspace.workingDirectory.path}
+            </h5>
           )}
         </Stack>
 
@@ -110,7 +125,7 @@ const HomePage = () => {
         <div
           style={{
             marginTop: "2rem",
-            padding: "2rem",
+            padding: "0.75rem",
             borderRadius: "8px",
             boxShadow: "0px 2px 5px rgba(128, 117, 117, 0.1)",
             textAlign: "left",
@@ -118,45 +133,55 @@ const HomePage = () => {
           }}
         >
           <h3 style={{ marginBottom: "1rem", color: "#4991dfff" }}>
-            Getting Started 🚀
+            {<Play size={24} className="ml-4 mb-1" />} Getting Started 
           </h3>
           
           <p>
-          To effectively navigate MEDiml and its functionalities, we recommend consulting the official documentation and tutorial resources.
-          These materials will help you understand how to analyze medical images using MEDiml, define and run experimentations, and evaluate machine learning models.
+            To effectively navigate MEDiml and its functionalities, we recommend consulting the official documentation and tutorial resources.
+            These materials will help you understand how to analyze medical images using MEDiml, define and run experimentations, and evaluate machine learning models.
           </p>
 
           <p>We provide dedicated tutorials and documentation to guide you step by step:</p>
 
           <ul style={{ paddingLeft: "1.5rem", listStyleType: "none" }}>
-            <li>📖  
-              <a href="https://medomicslab.gitbook.io/mediml-app-docs/" 
-                  target="_blank" rel="noopener noreferrer" style={{ color: "#4991dfff", textDecoration: "none", marginLeft: "5px" }}>
+            <li>
+              <FileText size={20} strokeWidth={1} className="ml-4 mb-1" /> 
+              <a 
+                href="https://medomicslab.gitbook.io/mediml-app-docs/" 
+                target="_blank" rel="noopener noreferrer" 
+                style={{ color: "#4991dfff", textDecoration: "none", marginLeft: "5px" }}
+              >
                 MEDiml Documentation
               </a>
             </li>
 
-            <li>🎥 
+            <li>
+              <MonitorPlay size={20} strokeWidth={1} className="ml-4 mb-1" />
               <a href="https://youtube.com/playlist?list=PLEPy2VhC4-D5Eg-UxRyTtmUZRh-D5m_Ru&si=n9lX4lRYfDci3v5V" 
                   target="_blank" rel="noopener noreferrer" style={{ color: "#4991dfff", textDecoration: "none", marginLeft: "5px" }}>
                 Video Tutorials
               </a>
             </li>
-            </ul>
+          </ul>
 
             {/* Warning section */}
             <div 
-            style={{
-              marginTop: "1rem",
-              padding: "1rem",
-              backgroundColor: "#97781bff",
-              borderLeft: "4px solid #ffc107",
-              borderRadius: "5px"
-            }}
-          >
-            ⚠️ MEDiml is part of the MEDomics platform, and only supports medical image analysis. 
-            To access other types of data analysis, please refer <a href="https://medomics.app/" 
-                  target="_blank" rel="noopener noreferrer" style={{ color: "#4991dfff", textDecoration: "none", marginLeft: "5px" }}>
+              style={{
+                marginTop: "1rem",
+                padding: "1rem",
+                backgroundColor: "rgb(219, 195, 123)",
+                borderLeft: "4px solid #ffc107",
+                borderRadius: "5px"
+              }}
+            >
+              <TriangleAlert className="mr-2 mb-2 inline-block" size={20} strokeWidth={1.5} /> MEDiml is part of the MEDomics platform, and only supports medical image analysis. 
+              To access other types of data analysis, please refer 
+              <a 
+                href="https://medomics.app/" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                style={{ color: "#4991dfff", textDecoration: "none", marginLeft: "5px" }}
+              >
                 here
               </a>.
           </div>
