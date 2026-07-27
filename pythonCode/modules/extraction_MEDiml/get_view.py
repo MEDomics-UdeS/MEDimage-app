c64b2e95b2cc0f6f09083422a37528c87a86c009
MahdiAll99 <mehdiwinneri01@gmail.com>
2026-07-27 15:52:43 -0400

    renamed MEDimlApp to MEDimlExtraction and cleaned some unused files

--- pythonCode/modules/extraction_MEDiml/get_view.py
+++ pythonCode/modules/extraction_MEDiml/get_view.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 sys.path.append(str(Path(os.path.dirname(os.path.abspath(__file__))).parent.parent))
 from med_libs.GoExecutionScript import GoExecutionScript, parse_arguments
-from med_libs.MEDimlApp.MEDimlExtraction import MEDimlExtraction
+from med_libs.MEDimlExtraction.MEDimlExtraction import MEDimlExtraction
 from med_libs.server_utils import go_print
 
 USE_RAM_FOR_EXPERIMENTS_STORING = 1
